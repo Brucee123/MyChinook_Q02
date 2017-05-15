460ecaa06e2dd188c198ce79e2aea2569a1c02df
Allan Anderson <allana@shaw.ca>
2017-05-14 23:43:27 -0700

    asd

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public  class Artist
+    {
+        [Key]
+        public int AritistId { get; set; }
+        public String Name { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ ChinookSolution/ChinookWeb/Contact.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Contact.aspx.cs" Inherits="Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your contact page.</h3>
+    <address>
+        Vincent <br />
+        IEC<br />
+        <abbr title="Phone">P:</abbr>
+        187460237970
+    </address>
+</asp:Content>
